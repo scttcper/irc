8f4a97ed38e6cade693da7ec43deb09447e950fc
Scott Cooper <scttcper@gmail.com>
2026-04-09 20:27:58 -0700

    feat: new release workflow, remove coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,5 @@ import { configDefaults, defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude, 'dist/**'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      exclude: ['dist/**'],
-    },
   },
 });
